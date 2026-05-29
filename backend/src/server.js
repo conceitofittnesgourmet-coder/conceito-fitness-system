1d9c7e2c35cb5f34f72641f5d19e4c7d917764fe
Windows <windows@example.com>
2026-05-29 17:43:09 -0300

    fix: cors socketio vercel

--- backend/src/server.js
+++ backend/src/server.js
@@ -41,24 +41,16 @@ const server =
 // SOCKET.IO
 // ==========================================
 
-const io =
-  new Server(server, {
-
-    cors: {
-
-      origin:
-        "http://localhost:5173",
-
-      methods: [
-        "GET",
-        "POST",
-        "PUT",
-        "DELETE"
-      ]
-
-    }
-
-  });
+const io = new Server(server, {
+  cors: {
+    origin: [
+      "https://conceito-fitness-system-7c2o.vercel.app",
+      "http://localhost:5173"
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true
+  }
+});
 
 global.io = io;
 app.set("io", io);
